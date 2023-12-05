6945d8a0da38ae16c8fafe56f5579519bb3d84c2
bradleysundberg <149118782+bradleysundberg@users.noreply.github.com>
2023-12-05 12:31:53 -0800

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+alert("Hello, world"); //This is a alert displaying the statement "Hello, world"//
+
+var A = "This is the beginning of the string" + " and this is the end of the string"; //This is a concatenated string//
+
+document.write(A) //This is the statement that displays the previous string//
+
+document.write(3+3) //This is a statement containing an expression//
