25869a2e63db3213f087df5172498829d7159d68
bradleysundberg <149118782+bradleysundberg@users.noreply.github.com>
2023-12-16 11:33:22 -0800

    Add files via upload

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function my_Dictionary() { //This is the name of the function//
+    var Animal = { //This is the variable and the characteristics of the variable//
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Labrador",
+        Age:5,
+        Sound:
+    };
+    document.getElementById("Dictionary") .innerHTML = Animal.Sound; //This is an event handler that causes the animal sound to display when clicked//
+}
